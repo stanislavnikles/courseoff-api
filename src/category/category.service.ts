8bc4a4b40e0342bbb48a39182aeb8d010bf84e0f
usertfs_stasnikles <bR4tDbi7vQwpbjA5njKe3>
2021-08-04 13:14:17 +0300

    remove subcategories in the last node

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -16,13 +16,16 @@ export class CategoryService {
     return this.categoryRepository.find().then(categories => {
       for (let i = 0; i < categories.length; i++) {
         map[categories[i].id] = i;
-        categories[i].subcategories = [];
       }
 
       for (let i = 0; i < categories.length; i++) {
         const node = categories[i];
         if (node.parentId !== null) {
-          categories[map[node.parentId]].subcategories.push(node);
+          const root = categories[map[node.parentId]];
+          if (!root.subcategories) {
+            root.subcategories = [];
+          }
+          root.subcategories.push(node);
         } else {
           roots.push(node);
         }
