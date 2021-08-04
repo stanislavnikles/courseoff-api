feab7071b55394dc480b44d4345456fc0bdf34a5
usertfs_stasnikles <bR4tDbi7vQwpbjA5njKe3>
2021-08-04 15:42:03 +0300

    add cors headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.port || 3000
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   await app.listen(port);
   console.log(`Server started on the port ${port}`);
 }
